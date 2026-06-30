85e89365a1274c0f8f33b3139df79cb381706dd1
Rameen zahra <zrameen211@gmail.com>
2026-06-30 14:19:32 +0500

    Fix import case for Appointment model

--- backend/controllers/medicalRecordController.ts
+++ backend/controllers/medicalRecordController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import MedicalRecord from "../models/medicalRecord";
-import Appointment from "../models/Appointment";
+import Appointment from "../models/appointment";
 
 /* ================= RESPONSE HELPER ================= */
 const sendResponse = <T>(
